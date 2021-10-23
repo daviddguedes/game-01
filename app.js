55afe22e475dc58433920a04b90a6b6748b6da3d
David Coelho <guedes0379@gmail.com>
2021-10-23 20:22:06 +0100

    Update player

--- app.js
+++ app.js
@@ -9,37 +9,51 @@
 
     let rightPressed = false;
     let leftPressed = false;
-    let pVelocity = 3;
+    let pVelocity = 8;
 
     function Player() {
         this.x = cWidth / 2;
         this.y = 370;
         this.w = 30;
         this.h = 60;
+        this.velocity = 0.25;
         this.moving = false;
         this.render = function () {
             ctx.fillStyle = 'orange';
             ctx.fillRect(this.x, this.y, this.w, this.h);
         }
+        this.goRight = function () {
+            pVelocity = pVelocity + this.velocity;
+            this.x += pVelocity;
+        }
+        this.goLeft = function () {
+            pVelocity = (pVelocity > 8) ? pVelocity - this.velocity : pVelocity;
+            player.x -= pVelocity;
+        }
     }
     const player = new Player();
 
     function Background() {
-        this.x = 0;
+        this.x = -1000;
         this.y = 0;
         this.w = bg.width;
         this.h = bg.height;
         this.render = function () {
             if (player.moving && rightPressed) {
-                ctx.drawImage(bg, this.x -= 10, this.y);
+                if (player.x >= (cWidth / 2)) {
+                    ctx.drawImage(bg, this.x -= pVelocity, this.y);
+                } else {
+                    ctx.drawImage(bg, this.x, this.y);
+                    player.goRight();
+                }
                 if (this.x <= -1000) {
                     this.x = 0;
                 }
                 player.moving = false;
             } else if (player.moving && leftPressed) {
-                ctx.drawImage(bg, this.x += 10, this.y);
-                if (this.x >= 1000) {
-                    this.x = 0;
+                ctx.drawImage(bg, this.x, this.y);
+                if (player.x > 50) {
+                    player.goLeft();
                 }
                 player.moving = false;
             } else {
@@ -60,10 +74,10 @@
     }
 
     function keyDownHandler(event) {
-        if (event.keyCode == 39) {
+        if (event.keyCode == 68) {
             rightPressed = true;
         }
-        else if (event.keyCode == 37) {
+        else if (event.keyCode == 65) {
             leftPressed = true;
         }
         player.moving = true;
